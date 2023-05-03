ca40166f261410a58be5f2efd67aba01ec790b46
whiskacz <pawelblasiak1985@gmail.com>
2023-05-03 21:09:32 +0200

    TS webapp

---
+++ dist/bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+console.log("aplikacja internetowa");
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93ZWJhcHAvLi9zcmMvaW5kZXgudHMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJhcGxpa2FjamEgaW50ZXJuZXRvd2FcIik7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
---
+++ dist/index.js
@@ -0,0 +1,1 @@
+console.log("aplikacja internetowa");
